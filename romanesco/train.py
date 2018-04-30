a2aceccb3a5a5c7ff358646f87a76e0f4cf4c93b
Tannon Kew <tannon@tannons-macbook-pro.home>
2018-04-30 19:18:46 +0200

    early stopping

--- romanesco/train.py
+++ romanesco/train.py
@@ -10,7 +10,9 @@ from romanesco import reader
 from romanesco.const import *
 from romanesco.vocab import Vocabulary
 from romanesco.compgraph import define_computation_graph
-
+####
+from romanesco.score import score
+import sys
 
 def train(data: str, epochs: int, batch_size: int, vocab_max_size: int,
           save_to: str, log_to: str, **kwargs):
@@ -39,8 +41,14 @@ def train(data: str, epochs: int, batch_size: int, vocab_max_size: int,
         session.run(tf.global_variables_initializer())
         # write logs (@tensorboard)
         summary_writer = tf.summary.FileWriter(log_to, graph=tf.get_default_graph())
+
+
         # iterate over training data `epoch` times
         for epoch in range(1, epochs + 1):
+            ###
+            iteration_perplexities = []
+            ###
+
             total_loss = 0.0
             total_iter = 0
             for x, y in reader.iterate(raw_data, batch_size, NUM_STEPS):
@@ -51,6 +59,21 @@ def train(data: str, epochs: int, batch_size: int, vocab_max_size: int,
                 total_iter += 1
                 if total_iter % 100 == 0:
                     logging.debug("Epoch=%s, iteration=%s", epoch, total_iter)
+
+                    ### Attempt to add early stop functionality.
+                    # For each 100th iteration, score perplexity on dev set. If the current iteration perplexity is greater than the last 5 perplexity scores in list 'iteration perplexities', model is starting to overfit.
+                    current_iter_per = score(dev, load_from, batch_size)
+                    iteration_perplexities.append(current_iter_per)
+                    if len(iteration_perplexity) >= 5:
+                        for i in iteration_perplexity[-5:]:
+                            if current_iter_per > i:
+                                perplexity = np.exp(total_loss / total_iter)
+                                logging.info("Lowest perplexity reached. Training has been early stopped. Perplexity on training data after epoch %s: %.2f", epoch, perplexity)
+                                saver.save(session, os.path.join(save_to, MODEL_FILENAME))
+                                sys.exit()
+
+                    ###
+
             perplexity = np.exp(total_loss / total_iter)
             logging.info("Perplexity on training data after epoch %s: %.2f", epoch, perplexity)
             saver.save(session, os.path.join(save_to, MODEL_FILENAME))
